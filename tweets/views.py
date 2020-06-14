3dd9d9a3c0b6824ddf19ff82652ef99b085314dd
hungnguyen289 <hungnguyen289@gmail.com>
2020-06-14 17:28:51 +0545

    23 Tweet Form by Hand 1:56:55

--- tweets/views.py
+++ tweets/views.py
@@ -14,6 +14,7 @@ def home_view(request,*args, **kwargs):
 
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
+    print('POST data is:', request.POST)
     if form.is_valid():
         obj = form.save(commit=False)
         # will do other form related logic
