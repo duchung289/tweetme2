09b6ee6efd2bd96cf138e25384acdac99e3d05db
hungnguyen289 <hungnguyen289@gmail.com>
2020-06-15 16:47:32 +0545

    29 Serialize Django Model Object 2:26:47

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,15 @@
+import random
 from django.db import models
 
 class Tweet(models.Model):
+    # Map to SQL data
     # id = models.AutoField(primary_key=True)
     content = models.TextField()
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
-
-
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "likes": random.randint(0, 200)
+        }
--- tweets/views.py
+++ tweets/views.py
@@ -27,7 +27,7 @@ def tweet_create_view(request, *args, **kwargs):
         obj.save()
 
         if request.is_ajax():
-            return JsonResponse({}, status = 201) #201 == created items
+            return JsonResponse(obj.serialize(), status = 201) #201 == created items
 
         if next_url and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
@@ -41,7 +41,7 @@ def tweet_list_view(request, *args, **kwargs):
     return json data
     """
     qs = Tweet.objects.all()
-    tweets_list = [{'id': x.id, 'content': x.content, 'likes': random.randint(0,99)} for x in qs]
+    tweets_list = [x.serialize() for x in qs]
     data = {
         "isUser": False,
         "response": tweets_list
