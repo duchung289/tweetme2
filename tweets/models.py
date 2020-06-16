0f2fdf848a1033bee10bc68361ea371e6a5c7124
hungnguyen289 <hungnguyen289@gmail.com>
2020-06-16 11:18:35 +0545

    33 Users & Tweets 2:49:28

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,14 @@
 import random
+from django.conf import settings
 from django.db import models
+
+User = settings.AUTH_USER_MODEL
 
 class Tweet(models.Model):
     # Map to SQL data
     # id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # many users and have many tweets
+    # user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) # this is for recording the history of deleted tweets (backup database)
     content = models.TextField()
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
