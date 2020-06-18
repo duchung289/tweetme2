9341ae1c983c8c9fe2ffb23b5f0e52116bde979a
hungnguyen289 <hungnguyen289@gmail.com>
2020-06-18 16:02:46 +0545

    47 Retweeting Logic 04:24:18

--- tweets/models.py
+++ tweets/models.py
@@ -12,6 +12,7 @@ class TweetLike(models.Model):
 class Tweet(models.Model):
     # Map to SQL data
     # id = models.AutoField(primary_key=True)
+    parent = models.ForeignKey("self", null = True, on_delete=models.SET_NULL)
     user = models.ForeignKey(User, on_delete=models.CASCADE) # many users and have many tweets
     # user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) # this is for recording the history of deleted tweets (backup database)
     likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through='TweetLike')
--- tweets/serializers.py
+++ tweets/serializers.py
@@ -9,7 +9,8 @@ TWEET_ACTION_OPTIONS = settings.TWEET_ACTION_OPTIONS
 class TweetActionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     action = serializers.CharField()
-
+    content = serializers.CharField(allow_blank=True, required=False)
+    
     def validate_action(self, value):
         value = value.lower().strip() # "like " --> "like"
         if not value in TWEET_ACTION_OPTIONS:
