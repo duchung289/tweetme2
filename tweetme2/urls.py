f93dd1417c62de515c8cb9f7785177762e035296
hungnguyen289 <hungnguyen289@gmail.com>
2020-06-18 11:18:27 +0545

    45 JavaScript Tweet Action Handler 04:11:02

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -21,7 +21,8 @@ from tweets.views import (
     tweet_detail_view, 
     tweet_list_view, 
     tweet_create_view,
-    tweet_delete_view
+    tweet_delete_view,
+    tweet_action_view
 )
 
 urlpatterns = [
@@ -30,5 +31,6 @@ urlpatterns = [
     path('tweets', tweet_list_view),
     path('create-tweet', tweet_create_view),
     path('tweets/<str:tweet_id>', tweet_detail_view),
+    path('api/tweets/action', tweet_action_view),
     path('api/tweets/<str:tweet_id>/delete', tweet_delete_view)
 ]
