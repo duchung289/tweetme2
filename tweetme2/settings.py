212d9ae49b834421c0ac46d8b97f8640bb32aaf6
hungnguyen289 <hungnguyen289@gmail.com>
2020-06-19 18:53:17 +0545

    55 Handling CORS and Invalid HOST_HEADER in Django 05:32:18

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -25,11 +25,12 @@ SECRET_KEY = 'fl!$!0kk%&)grk%mg&tf1!w@7ozu^y5a&_$s#hk($9=87vy(i('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.cfe.sh']
+ALLOWED_HOSTS = ['127.0.0.1', '.cfe.sh', 'localhost']
 
 LOGIN_URL = "/login"
 
 MAX_TWEET_LENGTH = 240
+
 TWEET_ACTION_OPTIONS = ['like', 'unlike', 'retweet']
 
 
@@ -43,16 +44,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # third party
+    'corsheaders',
     'rest_framework',
-    # internal
-    # 'templates'
-    'tweets',
+    'tweets'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -129,6 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_URLS_REGEX = r'^/api/.*$'
 
 DEFAULT_RENDERER_CLASSES = [
         'rest_framework.renderers.JSONRenderer',
