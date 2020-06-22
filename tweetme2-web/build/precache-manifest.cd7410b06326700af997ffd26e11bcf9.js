da25fafca5fd8598888f38079b60784da4965b81
hungnguyen289 <hungnguyen289@gmail.com>
2020-06-22 11:39:15 +0545

    63 Adjust the React Render Process 06:19:10

---
+++ tweetme2-web/build/precache-manifest.cd7410b06326700af997ffd26e11bcf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb12fc771b11a987479e458df15df0b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a5dca6922ee94b0e80c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "995d47db6af999cfe417",
+    "url": "/static/js/2.e01fd780.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e01fd780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a5dca6922ee94b0e80c",
+    "url": "/static/js/main.2d7f73bb.chunk.js"
+  },
+  {
+    "revision": "43cfd09573b07f1c8945",
+    "url": "/static/js/runtime-main.99d83fc2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
