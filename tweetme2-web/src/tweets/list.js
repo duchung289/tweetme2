3afcb029d3b5b8fb319a42eb0347ea47b5ffd529
hungnguyen289 <hungnguyen289@gmail.com>
2020-06-27 18:31:35 +0545

    98 Handling Pagination in React 10:50:29

--- tweetme2-web/src/tweets/list.js
+++ tweetme2-web/src/tweets/list.js
@@ -37,6 +37,24 @@ export function TweetList(props) {
         updateFinalTweets.unshift(tweets)
         setTweets(updateFinalTweets)
     }
+
+    const handleLoadNext = (event   ) => {
+        event.preventDefault()
+        if (nextUrl !== null){
+            const handleLoadNextResponse = (response, status) => {
+                if (status === 200) {
+                    setNextUrl(response.next)
+                    const newTweets = [...tweets].concat(response.results)
+                    setTweetsInit(newTweets)
+                    setTweets(newTweets)
+                } else {
+                    alert('There was an error!')
+                }
+            }
+            apiTweetList(props.username, handleLoadNextResponse, nextUrl)
+        }
+    }
+
     return <React.Fragment>{tweets.map((item, index)=>{
         return <Tweet 
         tweet={item} 
@@ -44,6 +62,6 @@ export function TweetList(props) {
         className='my-5 py-5 border bg-white text-dark' 
         key={`${index}-{item.id}`}/>
     })}
-    { nextUrl!== null && <button className='btn btn-outline-primary'>Load next</button>}
+    {nextUrl!== null && <button onClick={handleLoadNext} className='btn btn-outline-primary'>Load next</button>}
     </React.Fragment>
 }
--- tweetme2-web/src/tweets/lookup.js
+++ tweetme2-web/src/tweets/lookup.js
@@ -5,10 +5,13 @@ export function apiTweetCreate(newTweet, callback) {
     backendLookup("POST", "/tweets/create/", callback, {content: newTweet})
 }
   
-export function apiTweetList(username, callback) {
+export function apiTweetList(username, callback, nextUrl) {
     let endpoint = "/tweets/"
     if (username) {
         endpoint = `/tweets/?username=${username}`
+    }
+    if (nextUrl !== null && nextUrl !== undefined){
+        endpoint = nextUrl.replace("http://localhost:8000/api", "")
     }
     backendLookup("GET", endpoint, callback)  
 }
